79276b806829af9ab19636d876c4d7700cad97b8
Meghana-G-R-1999 <meghanagr999@gmail.com>
2024-07-26 20:21:54 +0530

    Add files via upload

--- with_function.py
+++ with_function.py
@@ -27,15 +27,30 @@ scissors = '''
 ---.__(___)
 '''
 
-#without using functions
+loop = True
+list = ["rock", "paper", "scissors"]
+user_input = input("I Would Like to select: ").lower()
 
-list = [rock, paper, scissors]
-user_input = int(input("what Would you Like to select?\n0 for rock\n1 for paper\n2 for scissors: "))
-if user_input == 0 or user_input == 1 or user_input == 2:
-    print(list[user_input])
-    computer = random.randint(0,2)
-    print(f"Computer selected: {computer}")
-    print(list[computer])
+def figures(input):
+    if input == "rock":
+        print(rock)
+        return True
+    elif input == "paper":
+        print(paper)
+        return True
+    elif input == "scissors":
+        print(scissors)
+        return True
+    else:
+        print("Invalid input")
+        return False
+
+loop = figures(user_input)  
+
+while loop:
+    computer = random.choice(list)
+    print("Computer selected: "+ computer)
+    figures(computer)
     if user_input == computer:
         print("Draw")
     elif user_input == "paper" and computer == "rock":
@@ -46,6 +61,4 @@ if user_input == 0 or user_input == 1 or user_input == 2:
         print("You Won")
     else:
         print("Computer Won")
-    
-else:
-    print("invalid input")
+    loop = False
